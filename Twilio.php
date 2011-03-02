9bd7fea31cd52a55a4814c6f3e703082c34566b1
Neuman Vong <neuman@ashmoremusic.com>
2011-03-02 10:40:42 -0800

    refactoring names

--- Twilio.php
+++ Twilio.php
@@ -1,63 +1,37 @@
 <?php
 
-require_once 'tiny_http.php';
+require_once dirname(__FILE__) . '/' . 'tiny_http.php';
 
-class TwilioClient extends Resource {
-  protected $http;
-  protected $version;
-  public function __construct(
-    $sid,
-    $token,
-    $ver = '2010-04-01',
-    $_http = NULL
-  ) {
-    $this->version = $ver;
-    $this->http = (NULL === $_http)
-      ? new TinyHttp("https://$sid:$token@api.twilio.com", array('debug' => TRUE))
-      : $_http;
-    $this->accounts = new ListResource('Accounts', $this);
-    $this->account = new InstanceResource($sid, $this->accounts);
-  }
-  public function get($path) {
-    list($status, $headers, $body) =
-      $this->http->get("/$this->version/$path.json");
-    if (200 <= $status && $status < 300) {
-      if ($headers['Content-Type'] == 'application/json') {
-        return json_decode($body);
-      } else throw new ErrorException('not json');
-    } else throw new ErrorException("$status: $body");
-  }
+interface DataProxy {
+  function receive($key);
+  function send($key, $value);
 }
 
-class DataProxy {
-  public function get($key) { throw new ErrorException; }
-  public function set($key, $value) { throw new ErrorException; }
-}
-
-class Resource extends DataProxy {
+abstract class Resource implements DataProxy {
   protected $proxy;
-  public function __construct(DataProxy $proxy) {
+	protected $name;
+  public function __construct($name, DataProxy $proxy) {
+		$this->name = $name;
     $this->proxy = $proxy;
   }
+  public function receive($sid) {
+    return $this->proxy->receive("$this->name/$sid");
+  }
+	public function send($key, $value) {
+		throw new ErrorException('not implemented');
+	}
 }
 
-class ListResource extends DataProxy {
-  public function __construct($name, $proxy) {
-    $this->name = $name;
-    $this->proxy = $proxy;
-  }
-  public function get($sid) {
-    return $this->proxy->get("$this->name/$sid");
-  }
+class ListResource extends Resource {
 }
 
 class InstanceResource extends Resource {
   protected $sid;
   protected $object;
-  public function __construct($sid, DataProxy $proxy) {
+  public function __construct($sid, $name, DataProxy $proxy) {
     $this->sid = $sid;
     $this->object = new stdClass;
-    parent::__construct($proxy);
+    parent::__construct($name, $proxy);
   }
   public function __set($key, $value) {
     $this->object->$key = $value;
@@ -65,8 +39,35 @@ class InstanceResource extends Resource {
   }
   public function __get($key) {
     if (!isset($this->object->$key)) {
-      $this->object = $this->proxy->get($this->sid);
+      $this->object = $this->proxy->receive($this->sid);
     }
     return $this->object->$key;
+  }
+}
+
+class TwilioClient extends Resource {
+  protected $http;
+  protected $version;
+  public function __construct(
+    $sid,
+    $token,
+    $version = '2010-04-01',
+    $_http = NULL
+  ) {
+    $this->version = $version;
+    $this->http = (NULL === $_http)
+      ? new TinyHttp("https://$sid:$token@api.twilio.com", array('debug' => TRUE))
+      : $_http;
+    $this->accounts = new ListResource('Accounts', $this);
+    $this->account = new InstanceResource($sid, 'Account', $this->accounts);
+  }
+  public function receive($path) {
+    list($status, $headers, $body) =
+      $this->http->get("/$this->version/$path.json");
+    if (200 <= $status && $status < 300) {
+      if ($headers['Content-Type'] == 'application/json') {
+        return json_decode($body);
+      } else throw new ErrorException('not json');
+    } else throw new ErrorException("$status: $body");
   }
 }
