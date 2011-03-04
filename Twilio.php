a56fbb24fbf34ef43d7858b89a5e432242bbec6e
Neuman Vong <neuman@ashmoremusic.com>
2011-03-04 03:33:51 -0800

    tab spaces

--- Twilio.php
+++ Twilio.php
@@ -9,29 +9,29 @@ interface DataProxy {
 
 abstract class Resource implements DataProxy {
   protected $proxy;
-	protected $name;
+  protected $name;
   public function __construct($name, DataProxy $proxy) {
-		$this->name = $name;
+    $this->name = $name;
     $this->proxy = $proxy;
   }
   public function receive($sid) {
     return $this->proxy->receive("$this->name/$sid");
   }
-	public function send($key, $value) {
-		throw new ErrorException('not implemented');
-	}
+  public function send($key, $value) {
+    throw new ErrorException('not implemented');
+  }
 }
 
 class ListResource extends Resource {
-	public function get($sid) {
-		return new InstanceResource($sid, substr($this->name, 0, -1), $this);
-	}
+  public function get($sid) {
+    return new InstanceResource($sid, substr($this->name, 0, -1), $this);
+  }
 
-	public function items() {
-		$page = $this->proxy->receive($this->name);
-		$name = strtolower($this->name);
-		return $page->$name;
-	}
+  public function items() {
+    $page = $this->proxy->receive($this->name);
+    $name = strtolower($this->name);
+    return $page->$name;
+  }
 }
 
 class InstanceResource extends Resource {
@@ -40,34 +40,34 @@ class InstanceResource extends Resource {
   public function __construct($sid, $name, DataProxy $proxy) {
     $this->sid = $sid;
     $this->object = new stdClass;
-		$this->object->sid = $sid;
+    $this->object->sid = $sid;
     parent::__construct($name, $proxy);
   }
-	public function setObject($object) {
-		$this->object = $object;
-	}
+  public function setObject($object) {
+    $this->object = $object;
+  }
   public function __get($key) {
     if (!isset($this->object->$key)) {
       $this->object = $this->proxy->receive($this->sid);
-			if (isset($this->object->subresource_uris)) {
-				foreach ($this->object->subresource_uris as $res => $uri) {
-					$type = basename($uri, '.' . pathinfo($uri, PATHINFO_EXTENSION));
-					$name = strtolower($type);
-					$this->$name = new ListResource($type, $this);
-				}
-			}
+      if (isset($this->object->subresource_uris)) {
+        foreach ($this->object->subresource_uris as $res => $uri) {
+          $type = basename($uri, '.' . pathinfo($uri, PATHINFO_EXTENSION));
+          $name = strtolower($type);
+          $this->$name = new ListResource($type, $this);
+        }
+      }
     }
-		return isset($this->$key)
-			? $this->$key
-			: (
-				isset($this->object->$key)
-				? $this->object->$key
-				: NULL
-			);
+    return isset($this->$key)
+      ? $this->$key
+      : (
+        isset($this->object->$key)
+        ? $this->object->$key
+        : NULL
+      );
   }
-	public function receive($path) {
+  public function receive($path) {
     return $this->proxy->receive("$this->sid/$path");
-	}
+  }
 }
 
 class TwilioClient extends Resource {
@@ -92,8 +92,8 @@ class TwilioClient extends Resource {
     if (200 <= $status && $status < 300) {
       if ($headers['Content-Type'] == 'application/json') {
         $object = json_decode($body);
-				//var_export($object);
-				return $object;
+        //var_export($object);
+        return $object;
       } else throw new ErrorException('not json');
     } else throw new ErrorException("$status: $body");
   }
